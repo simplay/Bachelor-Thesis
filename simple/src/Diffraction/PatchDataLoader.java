932558d08a2255575398bab03f733415446cb2e2
Michael Single <silent.simplay@gmail.com>
2013-05-20 23:36:58 +0200

    created new patch loader class in order to load precomputed patch date within an instance of this class in order to have a better organization of loading patch data into our java host application

---
+++ simple/src/Diffraction/PatchDataLoader.java
@@ -0,0 +1,11 @@
+package Diffraction;
+
+import Materials.Material;
+
+public class PatchDataLoader {
+	private Material material;
+	
+	public PatchDataLoader(Material material){
+		this.material = material;
+	}
+}
