489fc7d8a3261fcb2e1aaa12471febb441dc2be9
Michael Single <silent.simplay@gmail.com>
2013-12-10 11:26:47 +0100

    Issue #38: Added new shader
    + introduced adaptive, non-lin. lambda-sampling, taylor-gaussian
    shader-body. Implementation is still missing.

--- scene/src/Constants/ShaderPaths.java
+++ scene/src/Constants/ShaderPaths.java
@@ -14,7 +14,9 @@ public enum ShaderPaths {
 	tg_Vert("../jrtr/shaders/taylorGaussian/diffractionVPrecomp.vert"),
 //	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussianAllLambda.frag"),
 //	tg_Frag("../jrtr/shaders/taylorGaussian/taylorPQAllLambda.frag"),
-	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussReqLambda.frag"),
+//	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussReqLambda.frag"),
+	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussReqLambdaNonLin"),
+	 
 	
 	// debug
 	specular_Vert("../jrtr/shaders/debug/full_specular.vert"),
