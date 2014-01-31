bc97073a33ca01deb3d9e969229bec5b2f1d6c4a
Michael Single <silent.simplay@gmail.com>
2014-01-31 21:51:56 +0100

    New shader added: Sinc interpolation
    Initial version: maybe we have to refine the overall sampling - currently
    assuming there is a convolution window we may apply without introducing
    aliasing.

--- scene/src/Constants/ShaderPaths.java
+++ scene/src/Constants/ShaderPaths.java
@@ -13,7 +13,8 @@ public enum ShaderPaths {
 	// taylor Gauss
 	tg_Vert("../jrtr/shaders/taylorGaussian/diffractionVPrecomp.vert"),
 //	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussianAllLambda.frag"),
-	tg_Frag("../jrtr/shaders/taylorGaussian/taylorPQAllLambda.frag"),
+//	tg_Frag("../jrtr/shaders/taylorGaussian/taylorPQAllLambda.frag"),
+	tg_Frag("../jrtr/shaders/taylorGaussian/pqsincinterpol.frag"),
 //	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussReqLambda.frag"),
 //	tg_Frag("../jrtr/shaders/taylorGaussian/taylorGaussReqLambdaNonLin.frag"),
 	 
